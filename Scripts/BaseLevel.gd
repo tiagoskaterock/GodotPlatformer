28129cdaad1672b141d6e85e18de59c82cbe5fb9
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-11-28 16:30:32 -0300

    24. Coin Counter

--- Scripts/BaseLevel.gd
+++ Scripts/BaseLevel.gd
@@ -1,22 +1,41 @@
 extends Node2D
 
+signal coin_total_changed
+
 var player_scene = preload("res://Scenes/Player.tscn")
 var spawn_position = Vector2.ZERO
 var current_player_node = null
+var totalCoins = 0
+var colletedCoins = 0
 
 func _ready():	
 	spawn_position = $Player.global_position
 	register_player($Player)
+	coinTotalChanged(get_tree().get_nodes_in_group("coin").size())	
+	
+	
+func coinCollected():
+	colletedCoins += 1
+	print(totalCoins, " - " ,colletedCoins)
+	emit_signal("coin_total_changed", totalCoins, colletedCoins)	
+	
+	
+func coinTotalChanged(newTotal):
+	totalCoins = newTotal
+	emit_signal("coin_total_changed", totalCoins, colletedCoins)
+	
 	
 func register_player(player):
 	current_player_node = player
 	current_player_node.connect("died", self, "on_player_died", [], CONNECT_DEFERRED)
+	
 	
 func create_player():
 	var player_instance = player_scene.instance()
 	add_child_below_node(current_player_node, player_instance)
 	player_instance.global_position = spawn_position
 	register_player(player_instance)
+	
 	
 func on_player_died():
 	current_player_node.queue_free()
--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -3,9 +3,13 @@ extends Node2D
 func _ready():
 	$Area.connect("area_entered", self, "on_area_entered")
 	
+	
 func on_area_entered(_Area2):
 	$AnimationPlayer.play("pickup")
 	call_deferred("disable_pickup")
+	var baseLevel = get_tree().get_nodes_in_group("base_level")[0]
+	baseLevel.coinCollected()
+	
 	
 func disable_pickup():
 	$Area/Shape.disabled = true
